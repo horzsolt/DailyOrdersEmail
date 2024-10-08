4502bcb62ed954c05ec2a70dfec33713aac5384f
horzsolt <horzsolt2006@gmail.com>
2024-10-08 15:06:55 +0200

    The very first
    Add Configuration class, integrate log4net, refactor email gen
    
    * Configuration.cs: Added Configuration class for email and DB settings.
    * DailyOrdersEmail.csproj: Included Configuration.cs, Util.cs, and log4net.config.
    * Program.cs: Integrated log4net, refactored logging and email generation.
    * Util.cs: Added utility methods for file saving and DB value retrieval.
    * log4net.config: Configured log4net with console and file appenders.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
 using System.Text;
 using System.Timers;
 
@@ -12,17 +17,21 @@ namespace DailyOrdersEmail
     {
         private static Timer checkTimer;
         private static DateTime lastCheckTime;
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         static void Main(string[] args)
         {
-            // Set the last check time to when the application starts
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            log.Info("Application started.");
+
             lastCheckTime = DateTime.Now;
 
-            // Set up and start the timer (check every 5 minutes)
-            checkTimer = new Timer(300000); // 5 minutes in milliseconds
+            /*checkTimer = new Timer(1800000);
             checkTimer.Elapsed += CheckForNewRecords;
             checkTimer.AutoReset = true;
-            checkTimer.Enabled = true;
+            checkTimer.Enabled = true;*/
+
+            CheckForNewRecords(null, null);
 
             Console.WriteLine("Press [Enter] to exit the program...");
             Console.ReadLine();
@@ -32,7 +41,6 @@ namespace DailyOrdersEmail
         {
             try
             {
-                // Fetch connection details from environment variables
                 string connectionString = $"Server={Environment.GetEnvironmentVariable("VIR_SQL_SERVER_NAME")};" +
                                           $"Database={Environment.GetEnvironmentVariable("VIR_SQL_DATABASE")};" +
                                           $"User Id={Environment.GetEnvironmentVariable("VIR_SQL_USER")};" +
@@ -42,24 +50,52 @@ namespace DailyOrdersEmail
                 {
                     connection.Open();
 
-                    // Query to get new records since last check
-                    string query = "SELECT * FROM YourTable WHERE RecordDate > @LastCheck";
+                    string query = "SELECT TOP 1 * FROM dbo.DailyOrderMailConfig";
+                    Configuration config = new Configuration();
 
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        command.Parameters.AddWithValue("@LastCheck", lastCheckTime);
-
-                        SqlDataAdapter adapter = new SqlDataAdapter(command);
-                        DataTable newRecords = new DataTable();
-                        adapter.Fill(newRecords);
-
-                        if (newRecords.Rows.Count > 0)
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            // Generate the HTML email content
-                            string htmlEmailContent = GenerateHtmlEmail(newRecords);
+                            if (reader.Read())
+                            {
+                                config.LastCheckTime = reader.IsDBNull(0) ? (DateTime.Now) : reader.GetDateTime(0);
+                                config.MailServer = reader.IsDBNull(1) ? String.Empty : reader.GetString(1);
+                                config.MailSecrecy = reader.IsDBNull(2) ? String.Empty : reader.GetString(2);
+                                config.MailPassword = reader.IsDBNull(3) ? String.Empty : reader.GetString(3);
+                                config.MailSendTo = reader.IsDBNull(4) ? String.Empty : reader.GetString(4);
+                                config.MailSaveToFolder = reader.IsDBNull(5) ? String.Empty : reader.GetString(5);
+                                config.MailSelectStatement = reader.IsDBNull(6) ? String.Empty : reader.GetString(6);
+                                config.MailRetentionDays = reader.IsDBNull(7) ? 0 : reader.GetInt32(7);
 
-                            // Send the email with the new records
-                            SendEmail(htmlEmailContent);
+                                log.Debug($"Last check time: {config.LastCheckTime}");
+                                log.Debug($"Select stmt: {config.MailSelectStatement}");
+                            }
+                            else
+                            {
+                                log.Error("No records found in the DailyOrderMailConfig table.");
+                                return;
+                            }
+                        }
+                    }
+                    
+                    query = config.MailSelectStatement;
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@timestamp", config.LastCheckTime);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            DataTable dataTable = new DataTable();
+                            dataTable.Load(reader);
+
+                            if (dataTable.Rows.Count > 0)
+                            {
+                                GenerateHtmlEmail(dataTable, config);
+                            } else
+                            {
+                                log.Info("No new records found.");
+                            }
                         }
                     }
                 }
@@ -69,39 +105,113 @@ namespace DailyOrdersEmail
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                log.Error($"Error: {ex.Message}");
             }
         }
 
-        private static string GenerateHtmlEmail(DataTable newRecords)
+        private static void GenerateHtmlEmail(DataTable dataTable, Configuration config)
         {
+
+            int CID = 0;
+            string agentName = String.Empty;
+
+            int actual_CID = 0;
+            string actual_agentName = String.Empty;
+
+            int sum_Rend_Unit = 0;
+            int sum_Rabatt = 0;
+            int sum_Forgalom = 0;
+
             StringBuilder htmlBuilder = new StringBuilder();
 
-            htmlBuilder.Append("<h3>New Records:</h3>");
+            foreach (DataRow row in dataTable.Rows)
+            {
+                actual_CID = row["CID"] is DBNull ? 0 : (int)row["CID"];
+                actual_agentName = row["Nev"] is DBNull ? String.Empty : (string)row["Nev"];
+
+                if ((actual_CID != CID) || (actual_agentName != agentName))
+                {
+                    // Add summary
+
+                    htmlBuilder.Append("<tr>");
+                    htmlBuilder.Append($"<td></td>");
+                    htmlBuilder.Append($"<td></td>");
+                    htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(sum_Rend_Unit)}</td>");
+                    htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(sum_Rabatt)}</td>");
+                    htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(sum_Forgalom)}</td>");
+                    htmlBuilder.Append("</tr>");
+
+                    htmlBuilder.Append("</table>");
+
+                    Util.SaveStringBuilderToFile(htmlBuilder, Path.Combine(config.MailSaveToFolder, CID.ToString() + "_" + agentName + ".html"));
+                    htmlBuilder.Clear();
+                    CID = actual_CID;
+                    agentName = actual_agentName;
+                    sum_Forgalom = 0;
+                    sum_Rabatt = 0;
+                    sum_Rend_Unit = 0;
+
+                    AddHeader(htmlBuilder, row);
+                }
+
+                htmlBuilder.Append("<tr>");
+                htmlBuilder.Append($"<td>{Util.GetValueOrDefault<string>(row["Termek"])}</td>");
+                htmlBuilder.Append($"<td>{Util.GetValueOrDefault<float>(row["Kedv_Sz"])}</td>");
+                htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(row["Rend_Unit"])}</td>");
+                htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(row["Rabatt"])}</td>");
+                htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(row["Forgalom"])}</td>");
+                htmlBuilder.Append("</tr>");
+
+                sum_Rend_Unit += Util.GetValueOrDefault<int>(row["Rend_Unit"]);
+                sum_Rabatt += Util.GetValueOrDefault<int>(row["Rabatt"]);
+                sum_Forgalom += Util.GetValueOrDefault<int>(row["Forgalom"]);
+            }
+        }
+
+        private static void AddHeader(StringBuilder htmlBuilder, DataRow row)
+        {
+            // Add header
             htmlBuilder.Append("<table border='1'>");
             htmlBuilder.Append("<tr>");
-
-            // Add headers
-            foreach (DataColumn column in newRecords.Columns)
-            {
-                htmlBuilder.Append($"<th>{column.ColumnName}</th>");
-            }
-
+            htmlBuilder.Append($"<td>Rend_Datum</td>");
+            htmlBuilder.Append($"<td>{Util.GetValueOrDefault<DateTime>(row["Rend_Datum"])}</td>");
             htmlBuilder.Append("</tr>");
 
-            // Add rows
-            foreach (DataRow row in newRecords.Rows)
-            {
-                htmlBuilder.Append("<tr>");
-                foreach (var item in row.ItemArray)
-                {
-                    htmlBuilder.Append($"<td>{item}</td>");
-                }
-                htmlBuilder.Append("</tr>");
-            }
+            htmlBuilder.Append("<tr>");
+            htmlBuilder.Append($"<td>Nev</td>");
+            htmlBuilder.Append($"<td>{Util.GetValueOrDefault<string>(row["Nev"])}</td>");
+            htmlBuilder.Append("</tr>");
+
+            htmlBuilder.Append("<tr>");
+            htmlBuilder.Append($"<td>CID</td>");
+            htmlBuilder.Append($"<td>{Util.GetValueOrDefault<int>(row["CID"])}</td>");
+            htmlBuilder.Append("</tr>");
+
+            htmlBuilder.Append("<tr>");
+            htmlBuilder.Append($"<td>Ugyfel</td>");
+            htmlBuilder.Append($"<td>{Util.GetValueOrDefault<string>(row["Ugyfel"])}</td>");
+            htmlBuilder.Append("</tr>");
+
+            htmlBuilder.Append("<tr>");
+            htmlBuilder.Append($"<td>Helyseg</td>");
+            htmlBuilder.Append($"<td>{Util.GetValueOrDefault<string>(row["Helyseg"])}</td>");
+            htmlBuilder.Append("</tr>");
+
+            htmlBuilder.Append("<tr>");
+            htmlBuilder.Append($"<td>Nagyker</td>");
+            htmlBuilder.Append($"<td>{Util.GetValueOrDefault<string>(row["Nagyker"])}</td>");
+            htmlBuilder.Append("</tr>");
 
             htmlBuilder.Append("</table>");
-            return htmlBuilder.ToString();
+
+            htmlBuilder.Append("<table border='1'>");
+            htmlBuilder.Append("<tr>");
+            htmlBuilder.Append($"<th>Termek</th>");
+            htmlBuilder.Append($"<th>Kedv_Sz</th>");
+            htmlBuilder.Append($"<th>Rend_Unit</th>");
+            htmlBuilder.Append($"<th>Rabatt</th>");
+            htmlBuilder.Append($"<th>Forgalom</th>");
+            htmlBuilder.Append("</tr>");
         }
 
         private static void SendEmail(string htmlContent)
---
+++ Util.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyOrdersEmail
+{
+    public static class Util
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public static void SaveStringBuilderToFile(StringBuilder sb, string filePath)
+        {
+            log.Debug($"Saving to file: {filePath}");
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.Write(sb.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error($"An error occurred while saving to file: {ex.Message}");
+            }
+        }
+
+        public static T GetValueOrDefault<T>(object value)
+        {
+            if (value == DBNull.Value || value == null)
+            {
+                if (typeof(T) == typeof(string))
+                {
+                    return (T)(object)string.Empty;
+                }
+                else if (typeof(T) == typeof(int))
+                {
+                    return (T)(object)0;
+                }
+                else if (typeof(T) == typeof(DateTime))
+                {
+                    return (T)(object)DateTime.Now;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+
+            return (T)value;
+        }
+
+    }
+}
